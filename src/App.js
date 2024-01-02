7c6456e72689a51e77c115db6fb1a5431af7e76e
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-01-02 10:11:53 -0600

    Added drop down menu for drink categories

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
       <h1>Boba Generator🧋</h1>
       <button type="button">Give Me A Random Drink!</button>
+      <h2>OR Give Some Prefrences to Make a Drink</h2>
+      <h3>Choose a Drink Category</h3>
+      <select name="drink-categories" id="drink-categories">
+        <option value="milk tea">Milk Tea</option>
+        <option value="fruit tea">Fruit Tea</option>
+        <option value="fruit lemonade">Fruit Lemonade</option>
+        <option value="smoothie">Smoothie</option>
+      </select>
     </div>
   );
 }
