33d5ebdab19a925573d61fe08fc565f0c3a8d977
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-01-03 13:01:40 -0600

    Added yes or no radio buttons

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ function App() {
         <option value="fruit lemonade">Fruit Lemonade</option>
         <option value="smoothie">Smoothie</option>
       </select>
+      <h3>Would You like a topping?</h3>
+      {/*Do I need to put this in a form?*/}
+      <input type="radio" id="yes" name="topping_answer" value="YES"></input>
+      <label for="YES">YES</label><br></br>
+      <input type="radio" id="no" name="topping_answer" value="NO"></input>
+      <label for="NO">NO</label><br></br>
     </div>
   );
 }
