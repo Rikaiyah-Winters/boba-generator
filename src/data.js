0a7de11367d117b125257a8272874ffb7e20403e
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-01-06 20:48:01 -0600

    Added data.js file and added milk tea flavors

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const milkTea = [
+    "Sun's",
+    "Classic",
+    "Black Sugar",
+    "Taro",
+    "Mango",
+    "Coconut",
+    "Matcha",
+    "Thai Tea",
+    "Red Bean",
+    "Strawberry",
+    "Honeydew",
+    "Pineapple",
+    "Vanilla",
+    "Avocado"
+]
