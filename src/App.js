507e38b1226001d295819ea2e831e8877e6b09dd
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-01-30 15:18:49 -0600

    Added styling to tablet sizes

--- src/App.js
+++ src/App.js
@@ -50,13 +50,17 @@ class App extends Component {
     return (
       <div className="App" >
         <h1>Boba Generator🧋</h1>
-        <h2>🩵 Change the drink category for a new, random drink in that category.</h2>
-        <h2>🩵Press topping button for a new random topping!</h2>
-        <DrinkCategories flavorGenerator={this.flavorGenerator} />
-        <Topping generateChosenTopping={this.generateChosenTopping} />
-        <h3>{this.state.chosenFlavor}</h3>
-        <h3>🫧</h3>
-        <h3>{this.state.chosenTopping}</h3>
+        <div className="info">
+          <h2>🩵 Change the drink category for a new, random drink in that category.</h2>
+          <h2>🩵Press topping button for a new random topping!</h2>
+          <DrinkCategories flavorGenerator={this.flavorGenerator} />
+          <Topping generateChosenTopping={this.generateChosenTopping} />
+        </div>
+        <div className="info">
+          <h3>{this.state.chosenFlavor}</h3>
+          <h3>🫧</h3>
+          <h3>{this.state.chosenTopping}</h3>
+        </div>
       </div>
     );
   };
